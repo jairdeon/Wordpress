d819e5eeeedb2bc9144b335954832afc54ab0988
thenule <jair.deon@gmail.com>
2015-12-18 15:14:36 -0200

    Create Advanced Custom Fields.php

---
+++ Advanced Custom Fields.php
@@ -0,0 +1,11 @@
+// Adiciona Página de opções "Informações"
+if( function_exists('acf_add_options_page') ) {
+ 
+    $page = acf_add_options_page(array(
+        'page_title'    => 'Informações',
+        'menu_title'    => 'Informações',
+        'menu_slug'     => 'informacoes',
+        'capability'    => 'edit_posts',
+        'redirect'  => false
+    ));
+}
