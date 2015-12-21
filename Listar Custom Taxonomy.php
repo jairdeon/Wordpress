2a1080a7610e0350c06101d7b4b9645320ea46a6
thenule <jair.deon@gmail.com>
2015-12-21 11:01:30 -0200

    Create Listar Custom Taxonomy.php

---
+++ Listar Custom Taxonomy.php
@@ -0,0 +1,36 @@
+<?php
+// No lugar de "sua_custom_taxonomy", coloque sua custom taxnomy.
+$terms = get_terms('sua_custom_taxonomy'); 
+foreach ( $terms as $term ):
+// Abre o código
+$codigo .= '<li><a class="filters-a" data-filter=".';
+$codigo .= $term->name;
+$codigo .= '" href="#">';
+$codigo .= $term->name;
+// Fecha o código
+$codigo .= '</a></li>';
+endforeach;
+
+// Imprime o código
+echo $codigo;
+?>
+
+<!-- Versão para Sublime -->
+<snippet>
+	<content><![CDATA[
+\$terms = get_terms('${1: Sua custom Taxonomy}'); 
+foreach ( \$terms as \$term ):
+// Abre o código
+\$codigo .= '${2:Seu código HTMl}';
+\$codigo .= \$term->name;
+// Fecha o código
+\$codigo .= '${3: Fecha seu código HTML}';
+endforeach;
+
+// Imprime o código
+echo \$codigo;
+?>
+]]></content>
+	<tabTrigger>custom-taxonomy-foreach</tabTrigger>
+	<scope>source.php</scope>
+</snippet>
