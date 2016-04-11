fe43a87442e2b7df5616b40fc7916766a50e367c
Th3.Nule <jair.deon@gmail.com>
2016-04-11 09:25:29 -0300

    Update Exibe todos os itens de uma custom_taxonomy.php

--- Exibe todos os itens de uma custom_taxonomy.php
+++ Exibe todos os itens de uma custom_taxonomy.php
@@ -1,6 +1,8 @@
 <?php 
 // Exibe todos os itens de uma custom_taxonomy - slug_taxonomia
-$terms = get_terms('slug_taxonomia');
-echo '<ul>'; foreach ($terms as $term) { $term_link = get_term_link( $term, 'slug_taxonomia' ); if( is_wp_error( $term_link ) ) continue;
+// Altere abaixo para sua custom taxonomy.
+$categoria = 'sua_custom_taxonomy';
+$terms = get_terms($categoria);
+echo '<ul>'; foreach ($terms as $term) { $term_link = get_term_link( $term, $categoria); if( is_wp_error( $term_link ) ) continue;
 echo '<li><a href="' . $term_link . '">' . $term->name . '</a></li>';}
 echo '</ul>'; ?>
